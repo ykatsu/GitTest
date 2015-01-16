0da30c85d214ce26a6a5b3d86d20c68603e23802
Yoshihisa Katsumata <dev@ykatsu.com>
2015-01-16 15:44:45 +0900

    Initial Commit

---
+++ GitSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitSample
+//
+//  Created by Yoshihisa Katsumata on 2015/01/16.
+//  Copyright (c) 2015年 Yoshihisa Katsumata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
